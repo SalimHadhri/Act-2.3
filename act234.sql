6f0e4fb4890bd79a3109bc379fcf183b27cae9c6
Salim Hadhri <91907068+SalimHadhri@users.noreply.github.com>
2021-10-24 18:19:48 +0100

    Update act234.sql
    Check character and order ascendant for login field

--- act234.sql
+++ act234.sql
@@ -1,35 +1,8 @@
-CREATE TABLE new_table(
-	id_table_2 bigserial NOT null PRIMARY KEY ,
-	login_2 VARCHAR(10)  DEFAULT 'toto' NOT null,
-    choix_2 choix_multiple NOT null,
-    date_de_creation_2 DATE NOT null
-);
+CREATE TABLE new_table
+AS (SELECT *
+    FROM ft_table
+    WHERE login LIKE '%a%' 
+    ORDER BY login  
 
-
-create or replace  FUNCTION  check_statues()   RETURNS trigger AS $table_trigger$
-begin
-
-       
-        INSERT INTO new_table 
-        SELECT (login,choix, date_de_creation)
-        FROM ft_table;
-
-end;
-$table_trigger$ LANGUAGE plpgsql;
-
-
-
-
-CREATE or replace TRIGGER table_trigger 
-    BEFORE INSERT OR UPDATE ON new_table
-    FOR EACH ROW
-    WHEN  length(login_2) <10
-    EXECUTE PROCEDURE check_statues() ;
-
-
-
-
-
-
-
+    )
 ;
